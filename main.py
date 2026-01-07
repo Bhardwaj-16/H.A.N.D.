cfd85c98a7ead018ec38d6dc6564c73613d3423c
Bhardwaj-14 <bhardwaj.14.mit@gmail.com>
2026-01-07 18:22:53 +0530

    Added Zoom and made it very much smooth, responsive and accurate. Now the Zoom works faster as it just uses the distance between two index fingers

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import cv2
 import mediapipe as mp
 
 from packages.hand_landmarks import get_frame_and_landmarks
-from packages.gestures import detect_gesture
+from packages.gestures import detect_gestures, detect_zoom
 
 mp_draw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
+
+prev_zoom_dist = None
+baseline_zoom_dist = None
 
 while cap.isOpened():
     frame, result = get_frame_and_landmarks(cap)
@@ -13,15 +16,40 @@ while cap.isOpened():
         break
 
     gesture = "NONE"
+    finger_positions = None
 
     if result.multi_hand_landmarks:
+        if len(result.multi_hand_landmarks) == 2:
+            gesture, prev_zoom_dist, baseline_zoom_dist, finger_positions = detect_zoom(
+                result.multi_hand_landmarks[0],
+                result.multi_hand_landmarks[1],
+                prev_zoom_dist,
+                baseline_zoom_dist
+            )
+
+        else:
+            prev_zoom_dist = None
+            baseline_zoom_dist = None
+            for hand_landmarks in result.multi_hand_landmarks:
+                gesture = detect_gestures(hand_landmarks)
+
         for hand_landmarks in result.multi_hand_landmarks:
             mp_draw.draw_landmarks(
                 frame,
                 hand_landmarks,
                 mp.solutions.hands.HAND_CONNECTIONS
             )
-            gesture = detect_gesture(hand_landmarks)
+    
+    else:
+        prev_zoom_dist = None
+        baseline_zoom_dist = None
+
+    if finger_positions is not None:
+        h, w, _ = frame.shape
+        for finger_pos in finger_positions:
+            x, y = finger_pos
+            cx, cy = int(x * w), int(y * h)
+            cv2.circle(frame, (cx, cy), 10, (0, 165, 255), -1)  # Orange dot
 
     cv2.putText(
         frame,
